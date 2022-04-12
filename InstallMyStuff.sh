aa708727737f56f2e78fc0001f6fda4b65f2c218
Daniel von Trotha <daniel.vontrotha@icloud.com>
2022-04-12 09:06:40 -0400

    Adding Install script

---
+++ InstallMyStuff.sh
@@ -0,0 +1,5 @@
+#! /bin/zsh
+xcode-select --install
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install youtube-dl vim nano pinentry pinentry-mac cocoapods zsh zsh-autosuggestions zsh-syntax-highlighting neofetch curl wget htop lolcat tesseract ffmpeg ethereum bash 
+brew install --cask fig blender daisydisk github-desktop visual-studio-code bartender onyx hazel minecraft utm raycast microsoft-edge	
